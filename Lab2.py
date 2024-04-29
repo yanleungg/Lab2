4f15c1330050228ba87af618db08bd43e06d6b75
yanleung <yanleung.23@ichat.sp.edu.sg>
2024-04-29 15:27:25 +0800

    v1.2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,34 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+       
+def get_user_input():
+    user_input=input()
+    mylist=user_input.split(",")
+
+    for i in range(len(mylist)):
+        mylist.append(float(mylist[0]))
+        mylist.pop(0)
+        
+    print(mylist)
+    return mylist 
+
+def calc_average():
+    print("calc_average")
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+       
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+if __name__ == "__main__":
+ main()
+
