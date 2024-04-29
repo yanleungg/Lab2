1e181c916477b8daa924553ab4276f155482e44d
yanleung <yanleung.23@ichat.sp.edu.sg>
2024-04-29 15:31:06 +0800

    v1.2

--- bmi.py
+++ bmi.py
@@ -9,9 +9,14 @@ def calculate_bmi(height,weight):
 # Classify weight status based on BMI
     if bmi <18.5:
         print("Under Weight")
+        return -1
+
     elif 18.5<=bmi<=25.0:
         print("Normal Weight")
+        return 0
     else: 
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=67, height=1.76)
+
